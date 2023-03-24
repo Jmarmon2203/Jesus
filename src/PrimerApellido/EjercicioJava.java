d747ae7663d7fb21d197dfcbe02798ab77270fe3
usuario <usuario@lm-clase>
2023-03-24 15:18:37 +0100

    Con esta version creamos la primera rama a fecha de 23 de marzo de 2023

---
+++ src/PrimerApellido/EjercicioJava.java
@@ -0,0 +1,30 @@
+package PrimerApellido;
+
+import java.util.Scanner;
+
+public class EjercicioJava {
+    private Scanner teclado;
+    private int lado;
+    
+    public EjercicioJava(){
+        teclado = new Scanner(System.in);
+        System.out.print("Introduzca el valor del lado: ");
+        lado=teclado.nextInt();
+    }
+    
+    public void imprimirPerimetro(){
+        int perimetro=lado*4;
+        System.out.println("El perimetro es de: "+perimetro+" cm");
+    }
+    
+    public void imprimirSuperficie(){
+        int area=lado*lado;
+        System.out.println("La superficie es de: "+area+" cm2");
+    }
+    
+    public static void main(String[] args){
+    	EjercicioJava c = new EjercicioJava();
+        c.imprimirPerimetro();
+        c.imprimirSuperficie();
+    }
+}
